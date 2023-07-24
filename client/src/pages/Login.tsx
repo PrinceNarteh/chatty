d537fa492dae9ea5b954798634171deeac5949b5
Prince Narteh <prinarteh@gmail.com>
2023-07-24 01:13:35 +0000

    feature: style login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -25,11 +25,14 @@ const Login = () => {
   const submit: SubmitHandler<FormValue> = async (data) => {
     const toastId = toast.loading("Loading...");
     try {
-      const res = await login(data);
+      const res: any = await login(data);
       toast.dismiss(toastId);
-      toast.success("Login successful!");
-      console.log(res);
-      navigate("/");
+      if (res.data) {
+        toast.success("Login successful!");
+        navigate("/");
+      } else {
+        toast.error(res.error.data);
+      }
     } catch (error: any) {
       toast.dismiss(toastId);
       toast.error(error.response.data);
@@ -37,20 +40,36 @@ const Login = () => {
   };
 
   return (
-    <div>
-      Login
-      <form onSubmit={handleSubmit(submit)}>
+    <div className="h-screen w-screen flex flex-col justify-center space-y-5 items-center text-white max-w-sm mx-auto text-center">
+      <img src="/images/logo.png" alt="" className="w-10 mb-10" />
+      <h3 className="text-4xl">Login</h3>
+      <p className="text-gray-500">
+        Please enter your email and password to login and continue
+      </p>
+      <form onSubmit={handleSubmit(submit)} className="w-9/12 space-y-5">
         <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" {...register("email")} />
+          <input
+            type="email"
+            placeholder="Email"
+            {...register("email")}
+            className="bg-transparent border border-light-green p-2 rounded-md w-full"
+          />
           {errors["email"] && <p>{errors["email"].message}</p>}
         </div>
         <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" {...register("password")} />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+            className="bg-transparent border border-light-green p-2 rounded-md w-full"
+          />
           {errors["password"] && <p>{errors["password"].message}</p>}
         </div>
-        <input type="submit" value="Login" />
+        <input
+          type="submit"
+          value="Login"
+          className="bg-dark-green w-full py-2 rounded-md cursor-pointer font-bold"
+        />
       </form>
     </div>
   );
