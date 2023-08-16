fe08f788dcbc55108acd59ec7be3a0d091bf1abe
Prince Narteh <prinarteh@gmail.com>
2023-08-16 16:57:53 +0000

    feature: connect socket to the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:4000");
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    socket?.emit("sendMessage", message);
+
+    setMessage("");
+  };
 
   return (
     <>
       <h1>Chatty App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
     </>
   );
 }
