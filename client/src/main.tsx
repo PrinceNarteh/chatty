e19d029dc3dd64e7ef401109fb6f99487c7eaa41
Prince Narteh <prinarteh@gmail.com>
2023-08-20 15:26:21 +0000

    added redux integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistedStore = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <Router />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
---
+++ client/src/redux/features/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import appApi from "../appApi";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: {
+    addNotification: (state, { payload }) => {},
+    resetNotification: (state, { payload }) => {},
+  },
+
+  extraReducers: (builder) => {
+    //save user after register
+    builder.addMatcher(
+      appApi.endpoints.register.matchFulfilled,
+      (_, { payload }) => payload
+    );
+
+    // save user after login
+    builder.addMatcher(
+      appApi.endpoints.login.matchFulfilled,
+      (_, { payload }) => payload
+    );
+
+    // destroy user session
+    builder.addMatcher(appApi.endpoints.logout.matchFulfilled, () => null);
+  },
+});
+
+export const { addNotification, resetNotification } = userSlice.actions;
+export default userSlice.reducer;
